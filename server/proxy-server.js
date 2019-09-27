f528081e3b54147f3e85f118f876a85c60157677
Andrew Lau <alau12004@yahoo.com>
2019-09-27 15:33:41 -0700

    add get request to proxy

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -1,13 +1,27 @@
 const nr = require('newrelic');
 const express = require('express');
+const request = require('request');
 
 const app = express();
 const bodyParser = require('body-parser');
 
 const port = 3000;
 
+app.use('/loaderio-cc692639d4e1ff69455098872e1588a9', (req, res, next) => {
+  res.send('loaderio-cc692639d4e1ff69455098872e1588a9')
+})
 
 app.use('/:id/', express.static('public'));
+
+app.get('/api/:id/reservations', (req, res) => {
+  console.log(req.params)
+  request.get(`http://localhost:3002/api/${req.params.id}/reservations/`, function (error, response, body){
+    console.log('error:', error);
+    console.log('statusCode:', response && response.statusCode);
+    console.log('body:', body);
+    res.end()
+  })
+})
 
 
 app.listen(port, () => { console.log(`Proxy server listening on port ${port}`); });
